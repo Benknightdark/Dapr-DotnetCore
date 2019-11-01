76018e2f28bcb77de72ed0c2f4c2146e32971f4a
moon791017 <moon791017@gmail.com>
2019-11-01 15:28:35 +0800

    OK

---
+++ AService/Models/Models.cs
@@ -0,0 +1,37 @@
+namespace AService.Models
+{
+    using System.ComponentModel.DataAnnotations;
+
+    /// <summary>
+    /// Represents a transaction used by sample code.
+    /// </summary>
+    public class Transaction
+    {
+        /// <summary>
+        /// Gets or sets account id for the transaction.
+        /// </summary>
+        [Required]
+        public string Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets amount for the transaction.
+        /// </summary>
+        [Range(0, double.MaxValue)]
+        public decimal Amount { get; set; }
+    }
+     /// <summary>
+    /// Class representing an Account for samples.
+    /// </summary>
+    public class Account
+    {
+        /// <summary>
+        /// Gets or sets account id.
+        /// </summary>
+        public string Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets account balance.
+        /// </summary>
+        public decimal Balance { get; set; }
+    }
+}
